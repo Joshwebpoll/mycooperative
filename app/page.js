50a537a920765ccb90e5c9eedea4d274cbdb8167
joshwebpoll <joshharyomide@gmail.com>
2025-06-04 16:48:22 +0100

    update testimonials

--- app/page.js
+++ app/page.js
@@ -37,9 +37,9 @@ export default function Home() {
       <WhyChooseUs />
       <CountDown />
       <LoanSettings />
-      <Testimonials />
-      <CallToAction2 />
+      {/* <Testimonials /> */}
       <TestimonialsCarousel />
+      <CallToAction2 />
 
       <Footer />
     </div>
